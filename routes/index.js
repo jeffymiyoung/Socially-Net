1bcb98ac42bd948167aef7afbeac543d09ac8628
Jeffrey Young <jeffreymiyoung@aol.com>
2022-08-07 23:42:14 -0400

    added routes structure

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// Imports / Declarations
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// Routes
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('<h1>😝 404 Error!</h1>');
+})
+
+// Export for External
+module.exports = router;
