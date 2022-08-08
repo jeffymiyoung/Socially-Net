15ef18955fac25f7191e2484291d7c656bb46302
Jeffrey Young <jeffreymiyoung@aol.com>
2022-08-07 21:37:25 -0400

    added User Model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+// Imports
+const { Schema, model } = require('mongoose');
+
+// Create the User Schema
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username is required!',
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'E-mail is required!',
+            match: [/.+@.+\..+/]
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought',
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// Virtual called friendCount to retrieve friend.length of user's friend's
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+// Create User Model thru Schema
+const User = model('User', UserSchema);
+
+// Export for External
+module.exports = User;
